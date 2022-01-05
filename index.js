5c9f2e9cdb29141d5b578a1f54b5ba00facfc4ab
drhaax <vegito922@gmail.com>
2022-01-05 13:15:30 +0200

    changed gteetinf

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi : 'there'});
+    res.send({ hi : 'thereee'});
 });
 
 const PORT = process.env.PORT;
